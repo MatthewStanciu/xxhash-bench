47e5b6c557b33c289e4c2aba805d32a2b9f23521
Matthew Stanciu <mattbstanciu@gmail.com>
2024-06-07 17:37:38 -0700

    no array

--- test-hash-wasm.js
+++ test-hash-wasm.js
@@ -8,16 +8,6 @@ const iterations = 1000;
 
 const data = fs.readFileSync(filePath);
 
-function createHashPromises(iterations, data) {
-  const promises = [];
-  for (let i = 0; i < iterations; i++) {
-    promises.push(xxhash64(data));
-  }
-  return promises;
-}
-
-const promises = createHashPromises(iterations, data);
-
 const obs = new PerformanceObserver((items) => {
   console.log(items.getEntries()[0].duration + ' ms');
   performance.clearMarks();
@@ -26,7 +16,9 @@ obs.observe({ entryTypes: ['measure'] });
 
 (async () => {
   performance.mark('A');
-  await Promise.all(promises);
+  for (let i = 0; i < iterations; i++) {
+    await xxhash64(data);
+  }
   performance.mark('B');
   performance.measure('A to B', 'A', 'B');
 })();
