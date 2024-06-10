2b83299f283e9e5902286a5d9130303b8141f720
Matthew Stanciu <mattbstanciu@gmail.com>
2024-06-10 13:54:12 -0700

    Fix broken `xxhash-wasm` benchmark

--- test-all.js
+++ test-all.js
@@ -1,4 +1,3 @@
-//@ts-check
 const { benchmark } = require('./benchmark')
 const { XXHash64 } = require('xxhash-addon');
 const { xxh64: xxhash64 } = require('@node-rs/xxhash');
@@ -6,18 +5,22 @@ const { xxhash64: hashwasmxxhash64 } = require('hash-wasm');
 const xxhashwasm = require('xxhash-wasm');
 const { encantisxxh64 } = require('./encantis/encantis')
 
-function wrapXXhashAddon(key) {
+function xxHashAddon(key) {
   const seed = Buffer.alloc(8)
   const xxh64 = new XXHash64(seed)
   xxh64.update(Buffer.from(key))
   return xxh64.digest()
 }
 
+async function xxHashWasm(key) {
+  xxhashwasm().then(hasher => hasher.h64(key))
+}
+
 (async() => {
-  await benchmark(wrapXXhashAddon, 'xxhash-addon')
+  await benchmark(xxHashAddon, 'xxhash-addon')
   await benchmark(xxhash64, 'noders-xxhash')
   await benchmark(hashwasmxxhash64, 'hash-wasm')
-  await benchmark(xxhashwasm, 'xxhash-wasm')
+  await benchmark(xxHashWasm, 'xxhash-wasm')
   const encantisxxh = await encantisxxh64()
   await benchmark(encantisxxh, 'encantis')
 })()
