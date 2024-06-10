84bbf93565a3f544447e14b3a610fb682424db2e
Matthew Stanciu <mattbstanciu@gmail.com>
2024-06-10 13:46:44 -0700

    remove unusued code

--- benchmark.js
+++ benchmark.js
@@ -1,17 +1,11 @@
 //@ts-check
-const { performance, PerformanceObserver } = require('perf_hooks');
+const { performance } = require('perf_hooks');
 
 let baseKey = "5QmaHpcFCqBYmrTpHL7nqeYFzQjXXStfWtYkAeFpmwHsH3g/dpl_BL2W9QkrX96G6moXhqCM1XUx3xJg/basepath/images/projects/cancel-workflow-action.jpg"
 const iterations = 1000;
 const keys = Array.from({length: iterations}).map((_, i) => `${baseKey}-${i}`)
 
-module.exports.benchmark = async function benchmark(xxhash, name) {
-  const obs = new PerformanceObserver((items) => {
-    console.log(items.getEntries()[0].duration + ' ms');
-    performance.clearMarks();
-  });
-  obs.observe({ entryTypes: ['measure'] });
-  
+module.exports.benchmark = async function benchmark(xxhash, name) {  
   const startTime = performance.now()
   for (let i = 0; i < iterations; i++) {
     await xxhash(keys[i])
